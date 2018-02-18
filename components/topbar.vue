8e649f7267d90c2632947c2189b721579be5645b
stephanburger54 <stephanburger54@gmail.com>
2018-02-18 15:57:20 +0200

    made it so that when a component is in the view and is a button in the topbar, the button is disabled in the topbar

--- components/topbar.vue
+++ components/topbar.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="topbar">
 		<div class="buttons">
-			<button @click="main.ViewComp('home')" v-if="!main.viewList.includes('home')">Home</button>
-			<button @click="main.ViewComp('profile')" v-if="!main.viewList.includes('profile')">My Profile</button>
-			<button @click="main.ViewComp('newPost')" v-if="!main.viewList.includes('newPost')">New Post</button>
-			<button @click="main.ViewComp('settings')" v-if="!main.viewList.includes('settings')">Settings</button>
+			<button @click="main.ViewComp('home')" :class="{disabled:main.viewList.includes('home')}">Home</button>
+			<button @click="main.ViewComp('profile')" :class="{disabled:main.viewList.includes('profile')}">My Profile</button>
+			<button @click="main.ViewComp('newPost')" :class="{disabled:main.viewList.includes('newPost')}">New Post</button>
+			<button @click="main.ViewComp('settings')" :class="{disabled:main.viewList.includes('settings')}">Settings</button>
 		</div>
 	</div>
 </template>
